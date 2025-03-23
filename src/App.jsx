800375535b01b0b81d33a358fb2eac0d7b867b39
BigBen7 <benardsimon7@gmail.com>
2025-03-23 14:44:57 +0100

    remove unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Events from './pages/Events';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Pricing from './pages/Pricing';
-import Help from './pages/Help';
-import LandingLayout from "./components/LandingLayout";
-import AuthLayout from "./components/AuthLayout";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Pricing from "./pages/Pricing";
+import Help from "./pages/Help";
+import LandingLayout from "./components/Layout/LandingLayout";
+import AuthLayout from "./components/Layout/AuthLayout";
 import SignInForm from "./pages/auth/SignIn";
 import SignUpForm from "./pages/auth/SignUp";
 
 function App() {
   return (
     <>
-    <Routes>
-    {/* Landing pages with Navbar */}
-    <Route element={<LandingLayout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/help" element={<Help />} />
-    </Route>
+      <Routes>
+        {/* Landing pages with Navbar */}
+        <Route element={<LandingLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/help" element={<Help />} />
+        </Route>
 
-    {/* Auth pages without Navbar */}
-    <Route element={<AuthLayout />}>
-      <Route path="/signin" element={<SignInForm />} />
-      <Route path="/signup" element={<SignUpForm />} />
-    </Route>
-  </Routes>
-  </>
+        {/* Auth pages without Navbar */}
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
