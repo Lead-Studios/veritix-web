10ece55c882e0e17d17800096af222028a0573ae
ekumamatthew <ekumamatthew1@gmail.com>
2025-03-26 02:05:21 +0100

    resolve merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,24 @@
 // import SignUpForm from '../components/forms/SignUpForm';
 // import SignInForm from '../components/forms/SignInForm';
-import HowItWorksSection from '../components/HowItWorks';
+import HowItWorksSection from "../components/HowItWorks";
 
 import React from "react";
 import TrendingEvent from "../components/TrendingEvent";
 
 function Home() {
   return (
-
     <div className="flex flex-col relative w-full bg-transparent ">
- 
-    <div className="mx-auto">
-      <h1 className="mb-4 text-3xl font-bold text-gray-800">
-        Welcome to Our Website
-      </h1>
-      <img src="/Images/Vector.svg" className="absolute -top-52 right-0" />
-      <TrendingEvent />
-      <HowItWorksSection />
+      <div className="mx-auto w-full">
+        <h1 className="mb-4 text-3xl font-bold text-gray-800">
+          Welcome to Our Website
+        </h1>
+        <img src="/Images/Vector.svg" className="absolute -top-52 right-0" />
+        <TrendingEvent />
+        <HowItWorksSection />
 
-      {/* <SignInForm /> */}
-      {/* <SignUpForm /> */}
-    </div>
+        {/* <SignInForm /> */}
+        {/* <SignUpForm /> */}
+      </div>
     </div>
   );
 }
