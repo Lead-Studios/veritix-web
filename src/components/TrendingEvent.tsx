6f6955c51a57e8064c122a23bb4c52c74475d3c7
ekumamatthew <ekumamatthew1@gmail.com>
2025-03-26 10:00:33 +0100

    card  color

--- src/components/TrendingEvent.tsx
+++ src/components/TrendingEvent.tsx
@@ -5,30 +5,30 @@ type Props = {};
 
 function TrendingEvent({}: Props) {
   return (
-    <div className="h-auto w-full flex flex-col  bg-[#101428] px-4 py-12 items-center ">
-      <div className="flex flex-col items-center mt-12   w-full">
+    <div className="h-auto w-full flex flex-col  bg-[#101428] lg:px-4 py-8 lg:py-12 items-center ">
+      <div className="flex flex-col items-center mt-8 lg:mt-12   w-full">
         <div className="items-start max-w-[1400px]  px-4 mb-10 flex-col gap-4 lg:gap-0  lg:flex-row flex w-full">
           <div className="w-full ">
-            <p className="text-[#E7FDFF] text-[36px] font-semibold ">
+            <p className="text-[#E7FDFF] md:text-[28px] text-[24px] lg:text-[36px] font-semibold ">
               Trending Events
             </p>
-            <p className="font-normal text-base text-[#E7FDFF]">
+            <p className="font-normal text-sm lg:text-base text-[#E7FDFF]">
               Discover what is hot right now
             </p>
           </div>
           <div className="flex flex-wrap w-full gap-3 lg:gap-4 lg:items-end lg:justify-end">
-            <select className="bg-[#E7FDFF]  rounded-full lg:h-[43px] p-2 px-4">
-              <option className="text-sm  text-[#013237] font-normal lg:font-semibold">
+            <select className="bg-[#E7FDFF] leading-0 lg:text-sm text-xs rounded-full lg:h-[43px] p-2 lg:p-2 lg:px-4">
+              <option className="lg:text-sm text-xs leading-0  text-[#013237] font-normal lg:font-semibold">
                 Weekdays
               </option>
             </select>
-            <select className="bg-[#E7FDFF] rounded-full    lg:h-[43px] lg:px-4  p-2">
-              <option className="text-sm  text-[#013237] font-normal lg:font-semibold">
+            <select className="bg-[#E7FDFF] leading-0 rounded-full lg:text-sm text-xs p-2    lg:h-[43px] lg:px-4 ">
+              <option className="lg:text-sm text-xs leading-0  text-[#013237] font-normal lg:font-semibold">
                 Event Type
               </option>
             </select>
-            <select className="bg-[#E7FDFF] rounded-full  lg:h-[43px] lg:px-4  p-2">
-              <option className="text-sm text-[#013237] font-normal lg:font-semibold">
+            <select className="bg-[#E7FDFF] leading-0 rounded-full lg:text-sm text-xs  lg:h-[43px] lg:px-4  p-2">
+              <option className="lg:text-sm text-xs leading-0 text-[#013237] font-normal lg:font-semibold">
                 Any Category
               </option>
             </select>
@@ -38,7 +38,13 @@ function TrendingEvent({}: Props) {
         <div className="grid lg:grid-cols-3 xl:grid-cols-4 px-4  md:grid-cols-2 grid-cols-1 max-w-[1400px] justify-between items-center gap-8 w-full">
           {events.map((event, i) => (
             <div key={i} className="">
-              <TrendingCards />
+              <TrendingCards
+                amount={event.amount}
+                title={event.title}
+                type={event.type}
+                time={event.time}
+                color={event.color}
+              />
             </div>
           ))}
         </div>
@@ -59,18 +65,21 @@ const events = [
     title: "Meterverse Festival",
     time: "Sat, Oct 21 · 7:00 PM",
     amount: "From $50",
+    color: "bg-[#00FFA0] text-[#013237]",
   },
   {
     type: "music",
     title: "Meterverse Festival",
     time: "Sat, Oct 21 · 7:00 PM",
     amount: "From $50",
+    color: "bg-[#00FFA0] text-[#013237]",
   },
   {
     type: "music",
     title: "Meterverse Festival",
     time: "Sat, Oct 21 · 7:00 PM",
     amount: "From $50",
+    color: "bg-[#00FFA0] text-[#013237]",
   },
   {
     type: "music",
--- src/components/cards/TrendingCards.tsx
+++ src/components/cards/TrendingCards.tsx
@@ -1,21 +1,29 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-type Props = {};
+type Props = {
+  type: string;
+  amount: string;
+  title: string;
+  time: string;
+  color?: string;
+};
 
-function TrendingCards({}: Props) {
+function TrendingCards({ type, title, amount, time, color }: Props) {
   return (
     <motion.div
       className="h-[350px] rounded-[20px] relative flex items-center justify-center w-full bg-[#00FFA01A] hover:bg-gradient-to-b from-[#E7FDFF] to-[#000625] group transition-all duration-300"
-      whileHover={{ scale: 1.10 }}
-      transition={{ duration: 0.3 , ease: "easeInOut"}}
+      whileHover={{ scale: 1.1 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
     >
       <div className="z-10 px-[3%] flex-row flex absolute w-full justify-between top-5">
         <button
-          className="px-4 py-1 bg-[#013237] text-[#F6E8DF] rounded-[4px] font-bold text-sm transition-all duration-300
-          group-hover:bg-[#00FFA0] group-hover:text-[#013237]"
+          className={`px-4 py-1  ${
+            color ? color : "bg-[#013237] text-[#F6E8DF] "
+          }  rounded-[4px] font-bold text-sm transition-all duration-300
+          group-hover:bg-[#00FFA0] group-hover:text-[#013237]`}
         >
-          Music
+          {type}
         </button>
         <div className="flex flex-row gap-4 items-center">
           <img
@@ -37,17 +45,22 @@ function TrendingCards({}: Props) {
         className="w-full h-auto top-10 absolute transition-all duration-300 group-hover:opacity-80"
       />
 
-      <div className="w-[90%] top-[190px] bg-[#F6E8DF4D] backdrop-blur-md h-[130px] absolute rounded-[20px] shadow-sm transition-all duration-300 group-hover:bg-[#F6E8DF80]">
-        <div className="p-4">
-          <p className="text-lg text-[#013237] font-bold">Metaverse Festival</p>
+      <div className="w-[90%] lg:top-[200px] top-[210px] bg-[#F6E8DF4D] backdrop-blur-md h-[130px] absolute rounded-[20px] shadow-sm transition-all duration-300 group-hover:bg-[#F6E8DF80]">
+        <div className="px-3 py-2   flex flex-row gap-4 justify-between items-start w-full">
+          <p className=" text-base whitespace-nowrap leading-0  text-[#013237] font-bold">
+            {title}
+          </p>
+          <p className="font-normal text-sm leading-0 whitespace-normal text-[#013237] font-poly">
+            {time}
+          </p>
         </div>
-        <div className="p-4 flex flex-row items-center justify-between">
+        <div className="px-3 py-2 flex flex-row items-center justify-between">
           <div
             className="flex-row flex gap-2 items-center bg-[#E7FDFF] p-2 rounded-md border transition-all duration-300
             group-hover:border-[#00FFA0] group-hover:bg-transparent"
           >
             <img src="/Images/8679214_coupon_ticket_icon 1.svg" />
-            <p>From $50</p>
+            <p>{amount}</p>
           </div>
 
           <div
