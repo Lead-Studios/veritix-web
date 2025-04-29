d8fb9a66a8c6e0750c714d404c54aeab65e4f592
Ngozi Aguluka <melissacinta@gmail.com>
2025-04-29 10:54:07 +0100

    integrated axios and react-query, rstructured auth files, updated project configuration to accept jsx by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Events from "./pages/Events";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Pricing from "./pages/Pricing";
-import Help from "./pages/Help";
-import LandingLayout from "./components/Layout/LandingLayout";
-import AuthLayout from "./components/Layout/AuthLayout";
-import SignInForm from "./pages/auth/SignIn";
-import SignUpForm from "./pages/auth/SignUp";
+import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Pricing from './pages/Pricing';
+import Help from './pages/Help';
+import LandingLayout from './components/Layout/LandingLayout';
+import AuthLayout from './components/Layout/AuthLayout';
+import SignInForm from './pages/auth/SignIn';
+import SignUpForm from './pages/auth/SignUp';
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
-      <Routes>
-        {/* Landing pages with Navbar */}
-        <Route element={<LandingLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/upcoming-events" element={<Events />} />
-          <Route path="/how-it-works" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/help" element={<Help />} />
-        </Route>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          {/* Landing pages with Navbar */}
+          <Route element={<LandingLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/upcoming-events" element={<Events />} />
+            <Route path="/how-it-works" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/help" element={<Help />} />
+          </Route>
 
-        {/* Auth pages without Navbar */}
-        <Route element={<AuthLayout />}>
-          <Route path="/signin" element={<SignInForm />} />
-          <Route path="/signup" element={<SignUpForm />} />
-        </Route>
-      </Routes>
+          {/* Auth pages without Navbar */}
+          <Route element={<AuthLayout />}>
+            <Route path="/signin" element={<SignInForm />} />
+            <Route path="/signup" element={<SignUpForm />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import React from "react";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
 // 1) import the CSS
 import 'react-toastify/dist/ReactToastify.css';
-
 
 // 2) import components
 import { ToastContainer } from 'react-toastify';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Router>
       <App />
       <ToastContainer
@@ -27,5 +25,5 @@ createRoot(document.getElementById("root")).render(
         pauseOnHover
       />
     </Router>
-  </StrictMode>
+  </React.StrictMode>
 );
