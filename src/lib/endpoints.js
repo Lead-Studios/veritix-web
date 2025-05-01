899760de573660884351b94fd4f9c5d13320b214
Michael Ibinola <ibinolamichael1@gmail.com>
2025-05-01 22:12:04 +0100

    Revert "Feat/auth api integration"

--- src/lib/endpoints.js
+++
@@ -1,6 +0,0 @@
-export const apiEndpoints = {
-  login: '/auth/login',
-  register: '/auth/register',
-  logout: '/auth/logout',
-  refreshToken: '/auth/refresh-token',
-};
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import "./index.css";
+import App from "./App";
 // 1) import the CSS
 import 'react-toastify/dist/ReactToastify.css';
+
 
 // 2) import components
 import { ToastContainer } from 'react-toastify';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Router>
       <App />
       <ToastContainer
@@ -25,5 +27,5 @@ createRoot(document.getElementById('root')).render(
         pauseOnHover
       />
     </Router>
-  </React.StrictMode>
+  </StrictMode>
 );
