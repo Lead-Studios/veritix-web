56d5750df40e78ef385bd6fe5efe9519b9291a70
BigBen-7 <benardsimon7@gmail.com>
2026-01-21 02:04:53 +0100

    feat: add analytics and profile pages with initial content

---
+++ src/features/analyics/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Analytics page - event owner stats, ticket sales, etc. later wave!</div>;
+}
+
+export default page;
---
+++ src/features/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Profile page - user identity & walle later</div>;
+}
+
+export default page;
